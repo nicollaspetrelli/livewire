e463fb2ee7a19993b2bdef2f63a05d020a77797f
Nicollas <contatonicollasfeitosa@gmail.com>
2021-02-12 15:47:07 -0300

    Data Binding Parte 2

--- app/Http/Livewire/DataBinding.php
+++ app/Http/Livewire/DataBinding.php
@@ -7,6 +7,9 @@ use Livewire\Component;
 class DataBinding extends Component
 {
     public $name = "Nicollas";
+    public $age = 18;
+    public $show = false;
+    public $select = 'LARAVEL';
 
     public function render()
     {
--- resources/views/livewire/data-binding.blade.php
+++ resources/views/livewire/data-binding.blade.php
@@ -1,4 +1,23 @@
 <div>
-    <input wire:model="name" type="text" name="name">
+    <input wire:model.lazy="name" type="text" name="name">
     Meu nome é {{ $name }}
+
+    <br>
+
+    Mostrar idade
+    <input wire:model="show" type="checkbox">
+
+    @if($show)
+        Minha idade é {{ $age }}
+    @endif
+
+    <hr>
+
+    Dado selecionado: {{ $select }}
+    <select wire:model="select" name="" id="">
+        <option>PHP</option>
+        <option>LIVEWIRE</option>
+        <option>LARAVEL</option>
+    </select>
+
 </div>
