50582bf18f695fcc61293c895a1ab31709e2d499
Nicollas <contatonicollasfeitosa@gmail.com>
2021-02-14 14:11:33 -0300

    LifeCycle Hooks Parte 2

--- app/Http/Livewire/LifeCycleHooks.php
+++ app/Http/Livewire/LifeCycleHooks.php
@@ -8,18 +8,53 @@ use App\Models\User;
 
 class LifeCycleHooks extends Component
 {
-    // public $name = '';
-    // public $lastName = '';
-    // public $requestParam = '';
+    public $name = '';
+    public $lastName = '';
+    public $hydrate = '';
+    public $updated = '';
+    public $updating = '';
+    public $newUpdatedName = '';
 
-    public $users = [];
+    // public $users = [];
 
+    // Sempre é executado tipo um construtor
     public function mount(Request $request)
     {
-        // $this->name = "Nicollas";
-        // $this->lastName = "Feitosa";
+        $this->name = "Nicollas";
+        $this->lastName = "Feitosa";
         // $this->requestParam = $request->input('name');
-        $this->users = User::all();
+        // $this->users = User::all();
+    }
+
+    // É executado quando um elemento do componente é atualizado
+    public function hydrate()
+    {
+        $this->hydrate = now();
+    }
+
+    // É executado toda vez que um model é executado
+    public function updated()
+    {
+        $this->updated = now();
+    }
+
+    // Faz a mesma coisa que o updated porém é executado antes
+    public function updating()
+    {
+        $this->updating = now();
+        sleep(1);
+    }
+
+    // Método Mágico - Toda vez que o updated $name for executado esse método será executado
+    public function updatedName()
+    {
+        $this->newUpdatedName = 'O nome foi atualizado';
+    }
+
+    public function randomName()
+    {
+        $names = ['Nicollau', 'Nicollas', 'Feitosa'];
+        $this->name = $names[rand(0,2)];
     }
 
     public function render()
--- resources/views/livewire/life-cycle-hooks.blade.php
+++ resources/views/livewire/life-cycle-hooks.blade.php
@@ -1,11 +1,19 @@
 <div>
-    {{-- The name is {{ $name }}
-    <br>
-    The last name is {{ $lastName }}
-    <br>
-    The request param is {{ $requestParam }} --}}
 
-    <h2>Lista de Usuarios</h2>
+    he name is {{ $name }} <br/>
+    The last name is {{ $lastName }} <br/>
+
+    <input wire:model='name' type="text"><br/>
+    <input wire:model='lastName' type="text"><br/>
+
+    <button wire:click='randomName'>Atualizar Nome</button><br/>
+
+    @if($hydrate) hydrate - {{ $hydrate }} @endif <br/>
+    @if($updated) updated - {{ $updated }} @endif <br/>
+    @if($updating) updating - {{ $updating }} @endif <br/>
+    @if($newUpdatedName) property name - {{ $newUpdatedName }} @endif <br/>
+
+    {{-- <h2>Lista de Usuarios</h2>
 
     @if($users)
 
@@ -19,6 +27,6 @@
 
         Nenhum usuário registrado ...
 
-    @endif
+    @endif --}}
 
 </div>
